f2e749c21bc5919b9f3bc04d7e89d7169a5ee062
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-25 12:54:54 +0800

    chore: implemented listview inside a column

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:supagrocery/ui/widgets/dumb_widgets/app_padding.dart';
 
 import 'home_viewmodel.dart';
 
@@ -19,15 +20,31 @@ class HomeView extends ViewModelBuilderWidget<HomeViewModel> {
         onPressed: viewModel.toCreateGroceryView,
       ),
       body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 30),
-            Text(
-              'Welcome back ${viewModel.user!.name}',
-              style: TextStyle(
-                fontSize: 24,
+            AppHPadding(
+              child: Text(
+                'Welcome back ${viewModel.user!.name}',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
               ),
             ),
+            SizedBox(height: 30),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 25,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(index.toString()),
+                );
+              },
+            )
           ],
         ),
       ),
