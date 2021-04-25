194840dad3d15c5f5b01e3756aa45d787effe293
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-25 09:20:28 +0800

    refactor: simplified

--- lib/app/supabase_api.dart
+++ lib/app/supabase_api.dart
@@ -4,7 +4,4 @@ const String SUPABASE_URL = 'https://borayzhhitkyveigfijz.supabase.co';
 const String SUPABASE_SECRET =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxOTMwODI5MCwiZXhwIjoxOTM0ODg0MjkwfQ.Kk1ckyjzCB98aWyBPtJsoWuTsbq2wyYfiUxG7fH4yAg';
 
-class SupabaseApi {
-  static SupabaseClient get client =>
-      SupabaseClient(SUPABASE_URL, SUPABASE_SECRET);
-}
+final SupabaseClient supabase = SupabaseClient(SUPABASE_URL, SUPABASE_SECRET);
